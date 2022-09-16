4e7cd34c655ec38534d73712d619dc9aa06c00f7
Luca <luca@weve-got-it.com>
2022-09-16 12:42:26 +0200

    Added Content: Improving our map annotations.

--- Location.swift
+++ Location.swift
@@ -5,6 +5,7 @@
 //  Created by Luca Capriati on 2022/09/16.
 //
 
+import CoreLocation
 import Foundation
 
 struct Location: Identifiable, Codable, Equatable {
@@ -13,4 +14,14 @@ struct Location: Identifiable, Codable, Equatable {
     var description: String
     let latitude: Double
     let longitude: Double
+    
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    static let example = Location(id: UUID(), name: "Buckingham Palace", description: "Where Queen Elizabeth lives with dorgis", latitude: 51.501, longitude: -0.141)
+    
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
 }
